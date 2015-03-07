8e35fbd4cf5f9473d54e1844b2d2d4f0f1d09ae5
Nicholas Rutherford <zgithub@isomorphism.ca>
2015-03-07 16:26:04 -0600

    Low level summariser complete

--- hnSummarized/summarise.py
+++ hnSummarized/summarise.py
@@ -1,6 +1,7 @@
 import nltk.data
 import nltk
 import networkx as nx
+import pprint
 
 import matplotlib as plt
 TEXT_DIR = "./hnSummarized/text/"
@@ -23,16 +24,26 @@ for w1, w2 in [[words[i], words[i+1]] for i in xrange(len(words)-1)]:
     g.add_node(w2)
     g.add_edge(w1,w2)
 
-pairs = []
-for w in words:
-    print nx.degree(w)
-    #pairs.append(w, nx.degree(w))
+# Compute values
+pairs = {}
+for n in g.nodes():
+    pairs[n] = g.degree(n)
 
-print pairs
 
-#nx.draw(g)
 # Rank sentences
-"""
+
 sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
-print '\n---\n'.join(sent_detector.tokenize(rawText))
-"""
+sentList = sent_detector.tokenize(rawText)
+
+high_sent = []
+
+for sentence in sentList:
+    sWords = nltk.word_tokenize(sentence)
+    sValue = 0
+    for w in sWords:
+        sValue += pairs[w]
+    sValue = sValue / float(len(sWords))
+    high_sent.append((sentence, sValue))
+
+for x in sorted(high_sent, key=lambda x : x[1], reverse=True):
+    print x
