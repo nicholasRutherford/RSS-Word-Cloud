0d890012f7e627f73db4e990d31ae4f5d3c6e9fd
Nicholas Rutherford <zgithub@isomorphism.ca>
2015-03-08 12:15:47 -0600

    Split files into folders by date

--- hnSummarized/hnDownload.py
+++ hnSummarized/hnDownload.py
@@ -4,22 +4,38 @@ Author: Nicholas Rutherford
 
 import requests
 
+import os
+from datetime import datetime
 import urllib2
 import pprint
 
 
 HTML_DIR = "./hnSummarized/html/"
 BASE_URL = "https://hacker-news.firebaseio.com/v0/"
+HN_BASE = "https://news.ycombinator.com/item?id="
 CUT_OFF = 100
+
+alreadyGot = []
+# Get list of files you've already downloaded
+for folder in os.listdir(HTML_DIR):
+    for downFile in os.listdir(HTML_DIR + folder):
+        alreadyGot.append(int(downFile.split(".")[0]))
 
 # Get top stories
 r = requests.get(BASE_URL + "topstories.json")
-links =  r.json()[:CUT_OFF]
+rawLinks =  r.json()[:CUT_OFF]
+
+links = []
+for link in rawLinks:
+    if link not in alreadyGot:
+        links.append(link)
+    else:
+        print "Already have: ", link
 
 def isGoodStory(q):
     """Determins if a story is 'good'"""
-    if q["title"].lower().count(pdf) > 0:
-        return False
+    if q["title"].lower().count('haskell') > 0:
+        return True
     if q["score"] < 100:
         return False
     return True
@@ -36,15 +52,35 @@ for link in links:
     except KeyError:
         print KeyError.text()
 
+def getFolder(query):
+    d = datetime.fromtimestamp(query["time"]).date().isoformat()
+    path = HTML_DIR + d +"/"
+    try: 
+        os.makedirs(path)
+    except OSError:
+        if not os.path.isdir(path):
+            print "Error on making folder: ", d
+    return path
+
+
+
 # Download the links for good stories
 print "downloading..."
 for link in goodStories:
     try:
-        name = link["id"]
+        name = str(link["id"])
         name = name + ".html"
-        response = urllib2.urlopen(link["url"])
+
+        folder = getFolder(link)
+
+        # If it is an internal link, preface it with base url
+        if link["url"] == "":
+            response = urllib2.urlopen(HN_BASE + str(link["id"]))
+        else:
+            response = urllib2.urlopen(link["url"])
         rawHtml = response.read()
-        outFile = file(HTML_DIR + name, "w+")
+
+        outFile = file(folder + name, "w+")
         outFile.write(rawHtml)
         outFile.close()
     except (urllib2.HTTPError, ValueError):
--- hnSummarized/htmlParse.py
+++ hnSummarized/htmlParse.py
@@ -2,20 +2,40 @@
 Author: Nicholas Rutherford
 """
 
-from os import listdir
 from bs4 import BeautifulSoup
+import magic
+import os
+from os import listdir
 
 HTML_DIR = "./hnSummarized/html/"
 TEXT_DIR = "./hnSummarized/text/"
 
-for htmlFile in listdir(HTML_DIR):
-    rawFile = file(HTML_DIR + htmlFile, "r")
-    rawHtml = rawFile.read()
-    rawFile.close()
+def getFolder(folder, downFile):
+    path = TEXT_DIR + folder +"/"
+    fName = downFile.split(".")[0] + ".txt"
+    try: 
+        os.makedirs(path)
+    except OSError:
+        if not os.path.isdir(path):
+            print "Error on making folder: ", d
+    return path + fName
 
-    soup = BeautifulSoup(rawHtml, "html5lib")
-    rawText = soup.get_text()
-    rawText = rawText.encode("ascii", "ignore")
-    rawFile = file(TEXT_DIR + htmlFile.split(".")[0] + ".txt", "w+")
-    rawFile.write(str(rawText))
-    rawFile.close()
+for folder in listdir(HTML_DIR):
+    for downFile in listdir(HTML_DIR + folder):
+        # Make sure the files are html files
+        path = HTML_DIR + folder + "/" + downFile
+        fileDesc = magic.from_file(path)
+        if fileDesc.split(",")[0] == "HTML document":       
+            rawFile = file(path, "r")
+            rawHtml = rawFile.read()
+            rawFile.close()
+
+            soup = BeautifulSoup(rawHtml, "html5lib")
+            rawText = soup.get_text()
+            rawText = rawText.encode("ascii", "ignore")
+
+            dPath = getFolder(folder, downFile)
+            rawFile = file(dPath, "w+")
+            rawFile.write(str(rawText))
+            rawFile.close()
+            
