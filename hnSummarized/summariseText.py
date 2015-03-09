acdd725307776d05513c0641a7b359b6400afe36
Nicholas Rutherford <zgithub@isomorphism.ca>
2015-03-09 15:08:56 -0600

    Save summaries

--- hnSummarized/summariseText.py
+++ hnSummarized/summariseText.py
@@ -1,100 +1,41 @@
-from os import listdir
+import os
+
 from sentenceSelection import selectSentences
-import json
 
-INFO_DICT = "./hnSummarized/info.json"
+
 TEXT_DIR = "./hnSummarized/text/"
+SUM_DIR = "./hnSummarized/summaries/"
+NUM_SENTENCES = 10
 
-infoFile = file(INFO_DICT, "r")
-info = json.load(infoFile)
+def getFolder(folder, name):
+    path = SUM_DIR + folder +"/"
+    fName = name
+    try:
+        os.makedirs(path)
+    except OSError:
+        if not os.path.isdir(path):
+            print "Error on making folder: ", d
+    return path + fName
 
-OUT ="""
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <title>HN Summarized</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" type="text/css" href="custom.css">
-</head>
-
-<body>
-
-<div class="jumbotron">
-    <h1 class="text-center">HN Summarized</h1> 
-</div>
-"""
-
-ROW = '<div class="row">'
-ELE = """
-    <div class="col-sm-6">
-        <div class="container-fluid">
-"""
-ELE_END = """
-        </div>
-    </div>
-"""
-ROW_END = "</div>\n"
-
-PAGE_END = """
-</body>
-</html>
-"""
-
-def block(title, summary, article, comments):
-    ele = """
-    <div class="col-sm-6">
-        <div class="container-fluid">
-            <h3 class ="text-center">{0}</h3>  
-            <blockquote>
-                <p>
-                    {1}
-                </p>
-                <footer>
-                    <a href ="{2}">Article</a> | <a href="{3}">HN Comments</a>
-                </footer>
-            </blockquote>
-        </div>
-    </div>
-"""
-    return ele.format(title, summary, article, comments)
-
-
-ADD_ROW_BEFORE = True
-
-for folder in listdir(TEXT_DIR):
-    for downFile in listdir(TEXT_DIR + folder):
-        fileID = downFile.split(".")[0]
-
+for folder in os.listdir(TEXT_DIR):
+    for downFile in os.listdir(TEXT_DIR + folder):
         path = TEXT_DIR + folder + "/" + downFile
 
         # Load text
         rawFile = file(path, "r")
         rawText = rawFile.read()
         rawFile.close()
-        sumSents = selectSentences(rawText, 10)
 
-        if ADD_ROW_BEFORE:
-            OUT += ROW
+        # Summarise
+        summary = selectSentences(rawText, NUM_SENTENCES)
 
-        title = info[fileID]["title"].encode("ascii", "ignore")
-        summary = sumSents.encode("ascii", "ignore")
-        article = info[fileID]["url"].encode("ascii", "ignore")
-        comments = info[fileID]["comments"].encode("ascii", "ignore")
+        # Key Words
+        keyWordsList = ["One, two, three"]
+        keyWords = keyWordsList[0] + " | ".join(keyWordsList[1:])
 
-        OUT += block(title, summary, article, comments)
+        toSave = keyWords + "\n" + summary
 
-        if not ADD_ROW_BEFORE: #False
-            OUT += ROW_END
-            ADD_ROW_BEFORE = True
-        else:
-            ADD_ROW_BEFORE = False
-
-ofile = file("out.html", "w+")
-ofile.write(OUT)
-ofile.close()
-
+        savePath = getFolder(folder, downFile)
+        rawFile = file(savePath, "w+")
+        rawFile.write(toSave)
+        rawFile.close()
